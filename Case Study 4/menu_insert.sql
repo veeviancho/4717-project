b5b12aa113f4a8edd8cbc00b731a4c8557b9c409
veeviancho <veeviancho@gmail.com>
2021-10-10 22:03:45 +0800

    Commit

--- Case Study 4/menu_insert.sql
+++ Case Study 4/menu_insert.sql
@@ -1,8 +1,8 @@
 use f32ee;
 
 insert into coffee values
-  (1, "Just Java", "Endless Cup" 2.00),
-  (2, "Cafe au Lait", "Single" 2.00),
+  (1, "Just Java", "Endless Cup", 2.00),
+  (2, "Cafe au Lait", "Single", 2.00),
   (3, "Cafe au Lait", "Double", 3.00),
   (4, "Iced Cappucino", "Single", 4.75),
   (5, "Iced Cappucino", "Double", 5.75);
